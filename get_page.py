cc62b7009ad331ededd45ee8b0aa2453e63fa3fb
Divesh <f20170875@pilani.bits-pilani.ac.in>
2020-11-06 20:01:16 +0530

    urllib issue resolved

--- get_page.py
+++ get_page.py
@@ -1,8 +1,12 @@
+# helper function  
+#    input: url
+#    output: HTML content of that url
 def get_page(url):
     try:
-        import urllib
-        return urllib.urlopen(url).read()
+        import urllib.request
+        # python3 has urllib.request whereas python2 works with only urllib
+        return urllib.request.urlopen(url).read()
     except:
-        return ""
+        return "Error with Loading the page"
 
 print(get_page('https://www.google.com/'))
